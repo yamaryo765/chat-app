c56ef44ab0e01605f2514844458bd98a6b4d6952
yamaryo765 <77099054+yamaryo765@users.noreply.github.com>
2021-01-17 17:07:22 +0900

    結合テストの実装

--- db/migrate/20210114051901_create_messages.rb
+++ db/migrate/20210114051901_create_messages.rb
@@ -4,7 +4,6 @@ class CreateMessages < ActiveRecord::Migration[6.0]
       t.string  :content
       t.references :room, foreign_key: true
       t.references :user, foreign_key: true
-
       t.timestamps
     end
   end
